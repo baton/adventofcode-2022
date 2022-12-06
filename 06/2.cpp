adc42125170dc29d260b33e8240360c223acd609
Alexander Chinyakov <a.chinyakov@drweb.com>
2022-12-06 10:02:55 +0300

    day 6 part 2 solution

---
+++ 06/2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+
+int main( int argc, char * argv[] )
+{
+	if( argc < 2 )
+		return 0;
+
+	std::ifstream ifs( argv[ 1 ] );
+
+	std::string s;
+	ifs >> s;
+
+	std::string::const_iterator i( s.cbegin() );
+	for( ; i != s.cend() - 14; ++i )
+	{
+		std::string t( i, i + 14 );
+		std::sort( t.begin(), t.end() );
+		if( std::unique( t.begin(), t.end() ) == t.end() )
+			break;
+	}
+
+	auto const result( std::distance( s.cbegin(), i ) + 14 );
+
+	std::cout << "result = " << result << std::endl;;
+
+	return 0;
+}
