32a90dd2e6f82e4c162a84d49a9616f44dcef53f
Alexander Chinyakov <a.chinyakov@drweb.com>
2022-12-07 20:43:06 +0300

    day 7 part 1 solution

---
+++ 07/1.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <list>
+#include <map>
+#include <numeric>
+#include <memory>
+
+
+using file_t = std::pair< std::string, uint64_t >;
+
+struct dir_t
+{
+	dir_t( dir_t * p, std::string const & n )
+		: up( p )
+		, name( n )
+	{}
+
+	dir_t * up = nullptr;
+	std::string const name;
+	std::map< std::string, dir_t * > subs;
+	std::list< file_t > files;
+	uint64_t size = 0;
+};
+
+using fs_t = std::list< dir_t >;
+
+dir_t * cd( fs_t & fs, dir_t * current, std::string const & name )
+{
+	if( name == ".." )
+		return current->up;
+
+	std::map< std::string, dir_t * >::const_iterator i( current->subs.find( name ) );
+	if( i != current->subs.end() )
+		return i->second;
+
+	fs.emplace_back( current, name );
+	return current->subs.emplace( name, &fs.back() ).first->second;
+}
+
+uint64_t calc_sizes( dir_t * dir )
+{
+	dir->size =
+		std::transform_reduce(
+			dir->files.begin(), dir->files.end(),
+			0ULL,
+			std::plus{},
+			[]( file_t const & f ) -> uint64_t
+			{
+				return f.second;
+			}
+		)
+		+
+		std::transform_reduce(
+			dir->subs.begin(), dir->subs.end(),
+			0ULL,
+			std::plus{},
+			[]( auto & dd ) -> uint64_t
+			{
+				return calc_sizes( dd.second );
+			}
+		);
+
+	return dir->size;
+}
+
+
+int main( int argc, char * argv[] )
+{
+	if( argc < 2 )
+		return 0;
+
+	fs_t fs;
+	fs.emplace_back( nullptr, "/" );
+	dir_t * root( &fs.back() );
+	dir_t * curdir( root );
+
+	std::ifstream ifs( argv[ 1 ] );
+	while( !ifs.eof() )
+	{
+		std::string cmd;
+		std::getline( ifs, cmd );
+
+		if( cmd[ 0 ] == '$' )
+		{
+			if( cmd.substr( 0, 4 ) == "$ cd" )
+				curdir = cd( fs, curdir, cmd.substr( 5 ) );
+		}
+		else
+		{
+			if( cmd.substr( 0, 3 ) != "dir" )
+			{
+				char const * b( cmd.c_str() );
+				char * e( nullptr );
+				uint64_t const x( std::strtoull( b, &e, 10 ) );
+				++e;
+
+				curdir->files.emplace_back( e, x );
+			}
+		}
+	}
+
+	calc_sizes( root );
+
+	uint64_t const result{
+		std::transform_reduce(
+			fs.begin(), fs.end(),
+			0ULL,
+			std::plus{},
+			[]( dir_t const & d ) -> uint64_t
+			{
+				if( d.size > 100000 )
+					return 0;
+				else
+					return d.size;
+			}
+		)
+	};
+
+	std::cout << "result = " << result << std::endl;
+
+	return 0;
+}
